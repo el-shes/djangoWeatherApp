d6e3df00731116302ad17d63ce5d1c4523ecf5d6
rrfpCSIR <shes980@gmail.com>
2022-03-16 19:20:25 +0200

    implemented feathure to have multiple cities weather

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -3,20 +3,28 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from .models import City
 
 
 def weather_view(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid=110a6ca59b063163742ebf7ff21d3e04'
-    city = 'Kyiv'
+    city = 'Shanghai'
 
-    r = requests.get(url.format(city, 'metric')).json()
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
+    weather_data = []
 
-    context = {'city_weather': city_weather}
+    cities = City.objects.all()
+    for city in cities:
+
+        r = requests.get(url.format(city, 'metric')).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data}
 
     return render(request, 'weatherApp/weather.html', context)
