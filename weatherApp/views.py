a1651aeea1a469acf489145f94fa1f41972e70a4
rrfpCSIR <shes980@gmail.com>
2022-03-17 17:30:37 +0200

    City names validations added

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -11,9 +11,27 @@ def weather_view(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid=110a6ca59b063163742ebf7ff21d3e04'
     city = 'Shanghai'
 
+    err_msg = ''
+
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            # duplicate city check
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if not existing_city_count:
+                # checking valid city
+                r = requests.get(url.format(new_city, 'metric')).json()
+                print(r)
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist'
+            else:
+                err_msg = 'City already in the database'
+
+    print(err_msg)
 
     form = CityForm()
 
@@ -32,6 +50,6 @@ def weather_view(request):
         }
         weather_data.append(city_weather)
 
-    context = {'weather_data': weather_data, 'form' : form}
+    context = {'weather_data': weather_data, 'form': form}
 
     return render(request, 'weatherApp/weather.html', context)
